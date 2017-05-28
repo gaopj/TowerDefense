6a322b9f5cde818d23e133c1fbe4f428c0ecf830
gaopiji <448030133@qq.com>
2017-05-28 21:47:41 +0800

    增加了炮塔攻击和怪物血条等模块

--- Assets/Scripts/MapCube.cs
+++ Assets/Scripts/MapCube.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MapCube : MonoBehaviour {
 	[HideInInspector]
 	public GameObject turretGo;//保存当前Cube身上炮台
 	public GameObject buildEffect;
+
+	private Renderer renderers;
+
+	void Start(){
+		renderers = GetComponent<Renderer> ();
+	}
 	public void BuildTurret(GameObject turretPrefab){
 		if(turretPrefab!=null)
 		turretGo= GameObject.Instantiate (turretPrefab,transform.position,Quaternion.identity);
 		GameObject effect= GameObject.Instantiate (buildEffect,transform.position,Quaternion.identity);
 		Destroy (effect, 1);
+	}
+
+	void OnMouseEnter(){
+		if (turretGo == null&&!EventSystem.current.IsPointerOverGameObject()) {
+			renderers.material.color = Color.red;
+		}
+	}
+	void OnMouseExit(){
+		renderers.material.color = Color.white;
 	}
 }
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -24,18 +24,47 @@ public class Turret : MonoBehaviour {
 	public Transform firePosition;
 	public GameObject bulletPrefab;//子弹
 
+	public Transform head;
+
 	void Start(){
 		timer = attackRateTime;
 	}
 	void Update(){
 		timer += Time.deltaTime;
+		if (enemys.Count > 0 && enemys [0] != null) {
+			Vector3 targetPosition = enemys [0].transform.position;
+			targetPosition.y = head.position.y;
+			head.LookAt (targetPosition);
+		}
 		if (enemys.Count>0&& timer >= attackRateTime) {
-			timer -= attackRateTime;
+			timer =0;
 			Attack ();
 		}
+
 	}
 
 	void Attack(){
-		GameObject.Instantiate (bulletPrefab, firePosition.position, firePosition.rotation);
+		if (enemys [0] == null) {
+			UpdateEnemys ();
+		}
+		if (enemys.Count > 0) {
+			GameObject bullet = GameObject.Instantiate (bulletPrefab, firePosition.position, firePosition.rotation);
+			bullet.GetComponent<Bullet> ().SetTarget (enemys [0].transform);
+		} else {
+			timer = attackRateTime;
+		}
+	}
+
+	void UpdateEnemys(){
+		List<int> emptyIndex = new List<int>();
+		for (int i = 0; i < enemys.Count; i++) {
+			if (enemys [i] == null) {
+				emptyIndex.Add (i);
+			}
+		}
+
+		for (int i = 0; i < emptyIndex.Count; i++) {
+			enemys.RemoveAt (emptyIndex [i]-i);
+		}
 	}
 }
